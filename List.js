7dfbae54e8b0a6298a7d5ee1215d57995cc77f51
Aaron Templeton <aaron.templeton@live.com>
2019-06-09 19:26:12 -0600

    added getdata method

--- List.js
+++ List.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image, TextInput} from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import { Constants } from 'expo';
 
 export default class List extends React.Component {
-  constructor(props){
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  static navigationOptions = {
-      title: 'Results'
-  };
+    static navigationOptions = {
+        title: 'Results'
+    };
 
-  
+    state = {
+        isLoading: true
+    }
 
-  render(){
-      return(
-        <View>
-            <Text>List component</Text>
-        </View>
-      );
-  }
+    componentDidMount() {
+        this.getData();
+    }
+
+    type = this.props.navigation.getParam('selected');
+    location = this.props.navigation.getParam('location');
+    distance = this.props.navigation.getParam('distance');
+    number = this.props.navigation.getParam('number');
+
+    getData() {
+        return fetch('url')
+            .then((response) => response.json())
+            .then((responseJson) => {
+
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson
+                }, function () {
+
+                });
+            })
+            .catch((error)=>{
+                console.error(error);
+            });
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <ActivityIndicator color='#FA9D5C' size='large' />
+                </View>
+            );
+        }
+        return (
+            <View>
+                <Text>List component</Text>
+            </View>
+        );
+    }
 
 }
+
+const styles = StyleSheet.create({
+
+});
